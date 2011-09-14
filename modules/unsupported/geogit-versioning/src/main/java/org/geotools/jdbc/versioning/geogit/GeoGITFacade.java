957ee22cc0dc1a5ed618992b6aa958eb30eb9bd9
Mark Leslie <mark.s.leslie@gmail.com>
2011-09-14 17:27:53 +0930

    Migrating to the WrappedSerialisingFactory implementation.

--- modules/unsupported/geogit-versioning/src/main/java/org/geotools/jdbc/versioning/geogit/GeoGITFacade.java
+++ modules/unsupported/geogit-versioning/src/main/java/org/geotools/jdbc/versioning/geogit/GeoGITFacade.java
@@ -12,8 +12,8 @@ import org.geogit.api.ObjectId;
 import org.geogit.api.Ref;
 import org.geogit.repository.StagingArea;
 import org.geogit.repository.Triplet;
-import org.geogit.storage.FeatureWriter;
 import org.geogit.storage.ObjectWriter;
+import org.geogit.storage.WrappedSerialisingFactory;
 import org.geotools.util.NullProgressListener;
 import org.opengis.feature.Feature;
 import org.opengis.feature.type.Name;
@@ -24,12 +24,13 @@ import com.google.common.collect.Iterators;
 
 /**
  * @author wdeane
- *
+ * 
  */
 public class GeoGITFacade {
 
 	private GeoGIT ggit;
-    /**
+
+	/**
 	 * @param ggit
 	 */
 	public GeoGITFacade(GeoGIT ggit) {
@@ -38,89 +39,91 @@ public class GeoGITFacade {
 	}
 
 	/**
-     * Inserts the Feature to the index and stages it to be committed.
-     */
-    protected ObjectId insertAndAdd(Feature f) throws Exception {
-        ObjectId objectId = insert(f);
+	 * Inserts the Feature to the index and stages it to be committed.
+	 */
+	protected ObjectId insertAndAdd(Feature f) throws Exception {
+		ObjectId objectId = insert(f);
 
-        ggit.add().call();
-        return objectId;
-    }
+		ggit.add().call();
+		return objectId;
+	}
 
-    /**
-     * Inserts the feature to the index but does not stages it to be committed
-     */
-    protected ObjectId insert(Feature f) throws Exception {
-        final StagingArea index = ggit.getRepository().getIndex();
-        Name name = f.getType().getName();
-        String namespaceURI = name.getNamespaceURI();
-        String localPart = name.getLocalPart();
-        String id = f.getIdentifier().getID();
+	/**
+	 * Inserts the feature to the index but does not stages it to be committed
+	 */
+	protected ObjectId insert(Feature f) throws Exception {
+		final StagingArea index = ggit.getRepository().getIndex();
+		Name name = f.getType().getName();
+		String namespaceURI = name.getNamespaceURI();
+		String localPart = name.getLocalPart();
+		String id = f.getIdentifier().getID();
 
-        Ref ref = index.inserted(new FeatureWriter(f), f.getBounds(), namespaceURI, localPart, id);
-        ObjectId objectId = ref.getObjectId();
-        return objectId;
-    }
+		Ref ref = index.inserted(WrappedSerialisingFactory.getInstance().createFeatureWriter(f),
+				f.getBounds(), namespaceURI, localPart, id);
+		ObjectId objectId = ref.getObjectId();
+		return objectId;
+	}
 
-    protected void insertAndAdd(Feature... features) throws Exception {
-        insert(features);
-        ggit.add().call();
-    }
+	protected void insertAndAdd(Feature... features) throws Exception {
+		insert(features);
+		ggit.add().call();
+	}
 
-    protected void insert(Feature... features) throws Exception {
+	protected void insert(Feature... features) throws Exception {
 
-        final StagingArea index = ggit.getRepository().getIndex();
+		final StagingArea index = ggit.getRepository().getIndex();
 
-        Iterator<Triplet<ObjectWriter<?>, BoundingBox, List<String>>> iterator;
-        Function<Feature, Triplet<ObjectWriter<?>, BoundingBox, List<String>>> function = new Function<Feature, Triplet<ObjectWriter<?>, BoundingBox, List<String>>>() {
+		Iterator<Triplet<ObjectWriter<?>, BoundingBox, List<String>>> iterator;
+		Function<Feature, Triplet<ObjectWriter<?>, BoundingBox, List<String>>> function = new Function<Feature, Triplet<ObjectWriter<?>, BoundingBox, List<String>>>() {
 
-            @Override
-            public Triplet<ObjectWriter<?>, BoundingBox, List<String>> apply(final Feature f) {
-                Name name = f.getType().getName();
-                String namespaceURI = name.getNamespaceURI();
-                String localPart = name.getLocalPart();
-                String id = f.getIdentifier().getID();
+			@Override
+			public Triplet<ObjectWriter<?>, BoundingBox, List<String>> apply(
+					final Feature f) {
+				Name name = f.getType().getName();
+				String namespaceURI = name.getNamespaceURI();
+				String localPart = name.getLocalPart();
+				String id = f.getIdentifier().getID();
 
-                Triplet<ObjectWriter<?>, BoundingBox, List<String>> tuple;
-                ObjectWriter<?> writer = new FeatureWriter(f);
-                BoundingBox bounds = f.getBounds();
-                List<String> path = Arrays.asList(namespaceURI, localPart, id);
-                tuple = new Triplet<ObjectWriter<?>, BoundingBox, List<String>>(writer, bounds,
-                        path);
-                return tuple;
-            }
-        };
+				Triplet<ObjectWriter<?>, BoundingBox, List<String>> tuple;
+				ObjectWriter<?> writer = WrappedSerialisingFactory.getInstance().createFeatureWriter(f);
+				BoundingBox bounds = f.getBounds();
+				List<String> path = Arrays.asList(namespaceURI, localPart, id);
+				tuple = new Triplet<ObjectWriter<?>, BoundingBox, List<String>>(
+						writer, bounds, path);
+				return tuple;
+			}
+		};
 
-        iterator = Iterators.transform(Iterators.forArray(features), function);
+		iterator = Iterators.transform(Iterators.forArray(features), function);
 
-        index.inserted(iterator, new NullProgressListener(), null);
+		index.inserted(iterator, new NullProgressListener(), null);
 
-    }
+	}
 
-    /**
-     * Deletes a feature from the index
-     * 
-     * @param f
-     * @return
-     * @throws Exception
-     */
-    protected boolean deleteAndAdd(Feature f) throws Exception {
-        boolean existed = delete(f);
-        if (existed) {
-        	ggit.add().call();
-        }
+	/**
+	 * Deletes a feature from the index
+	 * 
+	 * @param f
+	 * @return
+	 * @throws Exception
+	 */
+	protected boolean deleteAndAdd(Feature f) throws Exception {
+		boolean existed = delete(f);
+		if (existed) {
+			ggit.add().call();
+		}
 
-        return existed;
-    }
+		return existed;
+	}
 
-    protected boolean delete(Feature f) throws Exception {
-        final StagingArea index = ggit.getRepository().getIndex();
-        Name name = f.getType().getName();
-        String namespaceURI = name.getNamespaceURI();
-        String localPart = name.getLocalPart();
-        String id = f.getIdentifier().getID();
-        boolean existed = index.deleted(namespaceURI, localPart, id);
-        return existed;
-    }
+	protected boolean delete(Feature f) throws Exception {
+		final StagingArea index = ggit.getRepository().getIndex();
+		Name name = f.getType().getName();
+		String namespaceURI = name.getNamespaceURI();
+		String localPart = name.getLocalPart();
+		String id = f.getIdentifier().getID();
+		boolean existed = index.deleted(namespaceURI, localPart, id);
+		return existed;
+	}
 
 }
