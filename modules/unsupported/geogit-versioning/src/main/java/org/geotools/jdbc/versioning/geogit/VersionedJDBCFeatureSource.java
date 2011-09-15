25d97535113c50ad1eb5579c67f3d41720efce99
unknown <wdeane@.ms.a2end.com>
2011-09-15 11:47:07 +1000

    changes to add logging

--- modules/unsupported/geogit-versioning/src/main/java/org/geotools/jdbc/versioning/geogit/VersionedJDBCFeatureSource.java
+++ modules/unsupported/geogit-versioning/src/main/java/org/geotools/jdbc/versioning/geogit/VersionedJDBCFeatureSource.java
@@ -7,7 +7,9 @@ import java.io.IOException;
 
 import org.geogit.api.GeoGIT;
 import org.geotools.data.Query;
+import org.geotools.data.collection.ListFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureCollection;
+import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.store.ContentEntry;
 import org.geotools.data.store.ContentFeatureSource;
 import org.geotools.jdbc.JDBCFeatureSource;
@@ -18,13 +20,12 @@ import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.filter.Filter;
 
+
 /**
  * @author wdeane
- * @param <F>
- * @param <T>
- * @param <F>
- * @param <T>
  *
+ * @param <T extends SimpleFeatureType>
+ * @param <F extends SimpleFeature>
  */
 @SuppressWarnings("unchecked")
 public class VersionedJDBCFeatureSource<T extends SimpleFeatureType, F extends SimpleFeature> extends JDBCFeatureSource<SimpleFeatureType, SimpleFeature> implements VersioningFeatureSource<SimpleFeatureType, SimpleFeature> {
@@ -52,9 +53,29 @@ public class VersionedJDBCFeatureSource<T extends SimpleFeatureType, F extends S
 	@Override
 	public SimpleFeatureCollection getLog(String fromVersion, String toVersion,
 			Filter filter, String[] userIds, int maxRows) throws Exception {
-		// TODO Auto-generated method stub
-		ggit.getLog(fromVersion, toVersion, this.buildFeatureType(), filter, maxRows);
-		return null;
+		// TODO Sanitise Filter
+		SimpleFeatureCollection sfc = ggit.getLog(fromVersion, toVersion, this.buildFeatureType(), filter, maxRows);
+		if (userIds != null && userIds.length>0) {
+			SimpleFeatureCollection sfc2 = new ListFeatureCollection(this.schema);
+			SimpleFeatureIterator iterator=sfc.features();
+			 try {
+			     while( iterator.hasNext()  ){
+			          SimpleFeature feature = iterator.next();
+			          for (String userid : userIds){
+			        	  if (userid.equals(feature.getAttribute("author"))){
+			        		  sfc2.add(feature);
+			        		  break;
+			        	  }
+			          }
+			     }
+			 }
+			 finally {
+				 iterator.close();
+			 }
+			 return sfc2;
+		}
+
+		return sfc;
 	}
 
 	@Override
